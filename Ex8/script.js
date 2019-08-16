020aa6743a9dd18d88cd6d6c69f12492db4d494e
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-08-16 00:32:16 -0300

    Correção exercicio 8

--- Ex8/script.js
+++ Ex8/script.js
@@ -1,7 +1,9 @@
 document.getElementById("botao").addEventListener("click",
   function() {
     var cpf = document.getElementById("cpf").value;
-
-    console.log(cpf.substr(1,3) + "." + cpf.substr(4,6) + "." + cpf.substr(7,9) + "-" cpf.substr(10,11));
+    //var.substr pega uma parte da string
+    // 123.456.789-10
+    // 012.345.678-910
+    console.log(cpf.substr(0,3) + "." + cpf.substr(3,3) + "." + cpf.substr(6,3) + "-" + cpf.substr(-2));
 
   });
