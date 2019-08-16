1bbbd78588f68cd57d3126001c11a1b1f2556455
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-08-16 00:05:05 -0300

    Exercicio 8 feito

--- Ex8/script.js
+++ Ex8/script.js
@@ -1,1 +1,7 @@
-document.getElementById("cpf")
+document.getElementById("botao").addEventListener("click",
+  function() {
+    var cpf = document.getElementById("cpf").value;
+
+    console.log(cpf.substr(1,3) + "." + cpf.substr(4,6) + "." + cpf.substr(7,9) + "-" cpf.substr(10,11));
+
+  });
